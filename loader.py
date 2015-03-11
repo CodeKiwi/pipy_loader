5280cbb6195daaff64263724e3ba1bac3a668a69
Josh Bondy <josh.bondy@codekiwi.com>
2015-03-12 06:28:20 +1100

    extend loader

--- loader.py
+++ loader.py
@@ -13,7 +13,8 @@ lcd.clear()
 lcd.message('Robot program loader')
 time.sleep(3.0)
 
-
+programs = ['test.py', 'test2.py', 'myprogram.py']
+selected = 0
 # Make list of button value, text, and backlight color.
 buttons = ( (LCD.SELECT, 'Select', (1,1,1)),
             (LCD.LEFT,   'Left'  , (1,0,0)),
@@ -23,12 +24,25 @@ buttons = ( (LCD.SELECT, 'Select', (1,1,1)),
 
 lcd.clear()
 lcd.message('Use buttons to\nselect program')
+
+def update_display():
+	line1 = programs[selected]
+	if (selected == len(programs)):
+		line2 = ''
+	else:
+		line2 = programs[selected+1]
+	lcd.clear()	
+	lcd.message(line1 + '\n' + line2)
+
+
 print 'Press Ctrl-C to quit.'
 while True:
 	# Loop through each button and check if it is pressed.
 	for button in buttons:
-		if lcd.is_pressed(button[0]):
-			# Button is pressed, change the message and backlight.
-			lcd.clear()
-			lcd.message(button[1])
-			lcd.set_color(button[2][0], button[2][1], button[2][2])
+		if lcd.is_pressed(LCD.UP):
+			if (selected > 0):
+				selected=-1
+		if lcd.is_pressed(LCD.DOWN):
+			if selected < len(programs):
+				selected=+1
+		update_display()
